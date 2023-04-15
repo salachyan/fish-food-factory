aa675b99180fd1ae360b224a22a3e51abc3cf651
yuinacats@gmail.com <yuinacats@gmail.com>
2023-04-15 12:54:56 -0400

    current_groceries.py

--- fish-food-factory-code/current_groceries.py
+++ fish-food-factory-code/current_groceries.py
@@ -2,10 +2,22 @@ from tkinter import *
 new_foods = ["lemon", "cilantro", "cookie"]
 root = Tk()
 root.geometry("400x400")
-root.title(" Q&A ")
 label = Text(root)
+  
+bgimg= PhotoImage(file = "fish-food-factory-code/background.png")
+limg= Label(root, i=bgimg)
+limg.place(x=0, y=0)
+limg.pack()
 for i in new_foods: 
     label.insert(END,i+'\n')
+    Font_tuple = ("Comic Sans MS", 20, "bold")
+    
+# Parsed the specifications to the
+# Text widget using .configure( ) method.
+    label.configure(font = Font_tuple)
+    label.tag_configure("center", justify='center')
+    label.insert(1.0, " ")
+    label.tag_add("center", "1.0", "end")
     label.pack()
 
  
