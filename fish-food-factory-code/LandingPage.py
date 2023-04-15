63f9349ca2329f487d22d674167bb8a6d1af05d4
anjalim8203 <123326959+anjalim8203@users.noreply.github.com>
2023-04-15 11:18:55 -0400

    fixed landing

--- fish-food-factory-code/LandingPage.py
+++ fish-food-factory-code/LandingPage.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+from tkinter import font
 
 class LandingPage(tk.Frame):
     def __init__(self, master=None):
@@ -10,20 +10,20 @@ class LandingPage(tk.Frame):
 
     def create_widgets(self):
         # Create a label for the landing page
-        self.label = tk.Label(self, text="Welcome to My Website!")
+        self.label = tk.Label(self, text="Welcome to the Fish Food Factory!", font=("Comfortaa", 20))
         self.label.pack(pady=20)
 
         # Create 4 buttons for different pages
-        self.button1 = tk.Button(self, text="Page 1", command=self.page1)
+        self.button1 = tk.Button(self, text="Page 1", command=self.page1, font=("Comfortaa", 16), bg="#FFE6E6", padx=20, pady=10)
         self.button1.pack(pady=10)
 
-        self.button2 = tk.Button(self, text="Page 2", command=self.page2)
+        self.button2 = tk.Button(self, text="Page 2", command=self.page2, font=("Comfortaa", 16), bg="#FFE6E6", padx=20, pady=10)
         self.button2.pack(pady=10)
 
-        self.button3 = tk.Button(self, text="Page 3", command=self.page3)
+        self.button3 = tk.Button(self, text="Page 3", command=self.page3, font=("Comfortaa", 16), bg="#FFE6E6", padx=20, pady=10)
         self.button3.pack(pady=10)
 
-        self.button4 = tk.Button(self, text="Page 4", command=self.page4)
+        self.button4 = tk.Button(self, text="Page 4", command=self.page4, font=("Comfortaa", 16), bg="#FFE6E6", padx=20, pady=10)
         self.button4.pack(pady=10)
 
     def page1(self):
@@ -46,7 +46,13 @@ class LandingPage(tk.Frame):
 if __name__ == "__main__":
     # Create a root window and set its title
     root = tk.Tk()
-    root.title("My Website")
+    root.title("Fish Food Factory")
+
+    # Set the window size
+    root.geometry("400x400")
+
+    # Set the window background color
+    root.configure(bg="#FFE6E6")
 
     # Create a landing page and display it
     landing_page = LandingPage(master=root)
