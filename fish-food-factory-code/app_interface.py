c96c1f08912b41e6abdf3dd68fbed1dad3a20bc0
Sara Inoue <sarainoue11@gmail.com>
2023-04-15 10:52:13 -0400

    added makewindow function

---
+++ fish-food-factory-code/app_interface.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+
+
+lst_of_foods = [1,2,3]
+
+#MAKE WINDOW
+
+window = tk.Tk()
+def makeWindow():
+    window.title("FishFoodFactory")
+    window.configure(width = 300, height = 500)
+
+
+def listFoods():
+    frame = tk.Frame()
+
+    for i in range(0,len(lst_of_foods)):
+        vari = tk.IntVar()
+        tk.Checkbutton(frame,text=lst_of_foods[i],variable=vari).grid(row=i)
+
+
+makeWindow()
+listFoods()
+
+
+
+#DISPLAY THE WINDOW
+window.mainloop()
